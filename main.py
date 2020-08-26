a67ed3c9b7ce4f183b15cf7434d9a1471b6ef2a1
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-08-26 22:49:12 +0300

    PEP 8 Refactoring

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import pytz, datetime
-
-from ics import Calendar, Event
+import datetime
+import pytz
 from colorama import Fore, Style
+from ics import Calendar, Event
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException
 
-from schedule import exist_group
-from exceptions import NoSuchGroupID
 from config import MAI_SCHEDULE_DETAIL
+from exceptions import NoSuchGroupID
+from schedule import exist_group
 
 
 def main():
-    group_id = input(Fore.WHITE + Style.BRIGHT + "Enter group id: " + Style.RESET_ALL)
+    group_id = input(Fore.WHITE + Style.BRIGHT + "Enter group id: "
+                     + Style.RESET_ALL)
     result_filename = f"{group_id}.ics"
     local = pytz.timezone("Europe/Moscow")
     study_calendar = Calendar()
 
     try:
-        print(Fore.WHITE, Style.BRIGHT, "Checking for the existence of a group...", Style.RESET_ALL, end="")
+        print(Fore.WHITE, Style.BRIGHT, "Checking for the existence of a "
+                                        "group...", Style.RESET_ALL, end="")
 
         if not exist_group(group_id):
             raise NoSuchGroupID(group_id)
@@ -32,14 +34,19 @@ def main():
 
         browser.get(base_url)
 
-        print(Fore.WHITE, Style.BRIGHT, "Number of university weeks:", Style.RESET_ALL, end="")
+        print(Fore.WHITE, Style.BRIGHT, "Number of university weeks:",
+              Style.RESET_ALL, end="")
 
-        number_study_weeks = len(browser.find_elements_by_css_selector(".table tr a")) # get all weeks
+        # get all weeks
+        number_study_weeks = len(
+            browser.find_elements_by_css_selector(".table tr a")
+        )
 
         print(number_study_weeks)
 
-        for i in range(1, number_study_weeks + 1): # iterates over weeks
-            print(Fore.WHITE, Style.BRIGHT, f"\nGetting {i} week schedule...", Style.RESET_ALL)
+        for i in range(1, number_study_weeks + 1):  # iterates over weeks
+            print(Fore.WHITE, Style.BRIGHT, f"\nGetting {i} week schedule...",
+                  Style.RESET_ALL)
 
             browser.get(f"{base_url}&week={i}")
 
@@ -52,7 +59,7 @@ def main():
                 day, month = stud_day.find_element_by_class_name(
                     "sc-day-header").text[: 5].split(".")
 
-                start_date = f"{year}-{month}-{day}" # YYYY-MM-DD
+                start_date = f"{year}-{month}-{day}"  # YYYY-MM-DD
 
                 items = stud_day.find_elements_by_css_selector(
                     ".sc-table-detail > .sc-table-row")
@@ -62,25 +69,28 @@ def main():
                     start_time, end_time = item.find_element_by_class_name(
                         "sc-item-time").text.split(" – ")
 
-                    event.name = item.find_element_by_class_name("sc-title").text # get title of study item
+                    event.name = item.find_element_by_class_name(
+                        "sc-title").text  # get title of study item
 
                     # convert local begin time to utc
-                    naive = datetime.datetime.strptime(f"{start_date} {end_time}", "%Y-%m-%d %H:%M")
+                    naive = datetime.datetime.strptime(
+                        f"{start_date} {end_time}", "%Y-%m-%d %H:%M")
                     local_dt = local.localize(naive, is_dst=None)
                     utc_dt = local_dt.astimezone(pytz.utc)
-                    event.end = utc_dt.strftime ("%Y-%m-%d %H:%M") 
+                    event.end = utc_dt.strftime("%Y-%m-%d %H:%M")
 
                     # convert local end time to utc
-                    naive = datetime.datetime.strptime(f"{start_date} {start_time}", "%Y-%m-%d %H:%M")
+                    naive = datetime.datetime.strptime(
+                        f"{start_date} {start_time}", "%Y-%m-%d %H:%M")
                     local_dt = local.localize(naive, is_dst=None)
                     utc_dt = local_dt.astimezone(pytz.utc)
-                    event.begin = utc_dt.strftime ("%Y-%m-%d %H:%M")
+                    event.begin = utc_dt.strftime("%Y-%m-%d %H:%M")
 
                     type_lesson = item.find_element_by_class_name(
                         "sc-item-type").text
 
                     location = item.find_element_by_class_name(
-                        "sc-item-location").text # get audience in MAI
+                        "sc-item-location").text  # get audience in MAI
 
                     # handle case when lecturer field is empty
                     try:
@@ -89,20 +99,24 @@ def main():
                     except NoSuchElementException:
                         lecturer = ''
 
-                    event.description = f"Type: {type_lesson}\nLocation: {location}\nLecturer: {lecturer}\n"
+                    event.description = f"Type: {type_lesson}\nLocation: " \
+                                        f"{location}\nLecturer: {lecturer}\n"
 
                     study_calendar.events.add(event)
 
-                print(Fore.WHITE, Style.BRIGHT, f'\t{start_date} -', Fore.GREEN, '\u2713', Style.RESET_ALL)
+                print(Fore.WHITE, Style.BRIGHT, f'\t{start_date} -', Fore.GREEN,
+                      '\u2713', Style.RESET_ALL)
 
         # save ics file
         with open(result_filename, "w") as ics_file:
             ics_file.writelines(study_calendar)
 
-        print(Fore.GREEN, Style.BRIGHT, f"\n  Done! Created {result_filename}\n", Style.RESET_ALL)
+        print(Fore.GREEN, Style.BRIGHT,
+              f"\n  Done! Created {result_filename}\n", Style.RESET_ALL)
 
     except NoSuchGroupID as e:
         print(Fore.RED, Style.BRIGHT, e, Style.RESET_ALL)
+
 
 if __name__ == "__main__":
     main()
